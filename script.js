1b482a79d19c6d0cf59973e2d66b1f79540c96c6
DESKTOP-7G1T89L <noepkn@gmail.com>
2024-05-17 18:20:14 -0400

    Completed Section 3 - Chapter 21-22

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twittenBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote(){
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    
+    // Check if Author field is blank and replace it with 'Unkown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    
+    // Check Quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes from API
@@ -18,6 +39,17 @@ async function getQuotes(){
     // Catch Error Here 
    }   
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listners
+newQuoteBtn.addEventListener('click',newQuote);
+twittenBtn.addEventListener('click', tweetQuote);
+
 
 // On Load
 getQuotes();
